021c4c6878f5ed740cf3aff65bf8b20a6cdf84fd
Maurílio Conceição <maurilio.conceicao@branef.com.br>
2021-05-05 17:58:42 -0300

    fim

--- lib/main.dart
+++ lib/main.dart
@@ -21,6 +21,9 @@ class _HomeState extends State<Home> {
   //Declaração da lista de array
   List _toDoList = [];
 
+  Map<String, dynamic> _lastRemoved;
+  int _lastRemovedPos;
+
   @override
   // ler os dados na inicialização da tela
   void initState() {
@@ -40,6 +43,21 @@ class _HomeState extends State<Home> {
       _toDoController.text = "";
       newToDo["ok"] = false;
       _toDoList.add(newToDo);
+      _saveData();
+    });
+  }
+
+  Future<Null> _refresh() async {
+    await Future.delayed(Duration(seconds: 1));
+    setState(() {
+      _toDoList.sort((a, b) {
+        if (a["ok"] && !b["ok"])
+          return 1;
+        else if (!a["ok"] && b["ok"])
+          return 1;
+        else
+          return 0;
+      });
       _saveData();
     });
   }
@@ -99,11 +117,13 @@ class _HomeState extends State<Home> {
                 ],
               )),
           Expanded(
+              child: RefreshIndicator(
+            onRefresh: _refresh,
             child: ListView.builder(
                 padding: EdgeInsets.only(top: 10.0),
                 itemCount: _toDoList.length,
                 itemBuilder: buildItem),
-          )
+          ))
         ],
       ),
     );
@@ -112,29 +132,54 @@ class _HomeState extends State<Home> {
   //Gera item da lista
   Widget buildItem(context, index) {
     return Dismissible(
-        key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
-        background: Container(
-          color: Colors.red,
-          child: Align(
-              alignment: Alignment(-0.9, 0.0),
-              child: Icon(
-                Icons.delete,
-                color: Colors.white,
-              )),
+      key: Key(DateTime.now().millisecondsSinceEpoch.toString()),
+      background: Container(
+        color: Colors.red,
+        child: Align(
+            alignment: Alignment(-0.9, 0.0),
+            child: Icon(
+              Icons.delete,
+              color: Colors.white,
+            )),
+      ),
+      direction: DismissDirection.startToEnd,
+      child: CheckboxListTile(
+        title: Text(_toDoList[index]["title"]),
+        value: _toDoList[index]["ok"],
+        secondary: CircleAvatar(
+          child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
         ),
-        direction: DismissDirection.startToEnd,
-        child: CheckboxListTile(
-          title: Text(_toDoList[index]["title"]),
-          value: _toDoList[index]["ok"],
-          secondary: CircleAvatar(
-            child: Icon(_toDoList[index]["ok"] ? Icons.check : Icons.error),
-          ),
-          onChanged: (value) {
-            setState(() {
-              _toDoList[index]["ok"] = value;
-              _saveData();
-            });
-          },
-        ));
+        onChanged: (value) {
+          setState(() {
+            _toDoList[index]["ok"] = value;
+            _saveData();
+          });
+        },
+      ),
+      onDismissed: (direction) {
+        setState(() {
+          _lastRemoved = Map.from(_toDoList[index]);
+          _lastRemovedPos = index;
+          _toDoList.removeAt(index);
+
+          _saveData();
+
+          final snack = SnackBar(
+            content: Text("Terefa \"${_lastRemoved["title"]}\" removida!"),
+            action: SnackBarAction(
+              label: "Desfazer",
+              onPressed: () {
+                setState(() {
+                  _toDoList.insert(_lastRemovedPos, _lastRemoved);
+                  _saveData();
+                });
+              },
+            ),
+            duration: Duration(seconds: 2),
+          );
+          Scaffold.of(context).showSnackBar(snack);
+        });
+      },
+    );
   }
 }
