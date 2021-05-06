ede31476e030e808ad465f93387d836b4f07fbcb
Maurílio Conceição <maurilio.conceicao@branef.com.br>
2021-05-06 10:45:36 -0300

    ajuste snakbar

--- lib/main.dart
+++ lib/main.dart
@@ -177,6 +177,7 @@ class _HomeState extends State<Home> {
             ),
             duration: Duration(seconds: 2),
           );
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
       },
